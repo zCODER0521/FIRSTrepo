9975b04e760778b7acd6410bf5a73353cffe8d59
zCODER0521 <nidhish.deshmukh050404@gmail.com>
2024-12-09 08:42:49 +0530

    restapi changes

---
+++ python.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI 
+from pydantic import BaseModel,Field 
+from uuid import UUID 
+app = FastAPI() 
+Books = []  
+class Book (BaseModel):
+     id:UUID  
+     title: str= Field(min_length=1)
+     author: str= Field(min_length=1,max_length=100) 
+     rating: int= Field(gt=-1,lt=101) 
+@app.get("/") 
+def root():  
+    return {"hello":"Nidhish"}      
+@app.post("/posts") 
+def create_book(book: Book):
+     Books.append(book) 
+     return book  
